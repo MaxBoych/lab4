79692bb02ae7e9d211c66b15b3eb7dec4090c802
maxboychuk <maxboydad@gmail.com>
2019-11-14 22:58:40 +0300

    Scripted auto-commit on change (2019-11-14 22:58:40) by gitwatch.sh

--- src/main/java/StoreActor.java
+++ src/main/java/StoreActor.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+import java.util.HashMap;
+import java.util.Map;
 
-public class StoreActor {
+public class StoreActor extends AbstractActor {
+    private Map<String, String> store = new HashMap<>();
+    @Override
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match(StoreMessage.class, m -> {
+                    store.put(m.getKey(), m.getValue());
+                    System.out.println("receive message! "+m.toString());
+                })
+                .match(GetMessage.class, req -> sender().tell(
+                        new StoreMessage(req.getKey(), store.get(req.getKey())), self())
+                ).build();
+    }
 }
