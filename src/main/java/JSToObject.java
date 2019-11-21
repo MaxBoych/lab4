3226846875eab1758c68778824f2686c70b75ed6
maxboychuk <maxboydad@gmail.com>
2019-11-22 01:57:06 +0300

    Scripted auto-commit on change (2019-11-22 01:57:06) by gitwatch.sh

--- src/main/java/JSToObject.java
+++ src/main/java/JSToObject.java
@@ -1,6 +1,27 @@
+import java.awt.*;
+import java.util.ArrayList;
+
 public class JSToObject {
 
     private String id;
     private String js;
-    private  String
+    private ArrayList<JSTest> tests;
+
+    public  JSToObject(String id, String js, ArrayList<JSTest> tests) {
+        this.id = id;
+        this.js = js;
+        this.tests = tests;
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public String getJS() {
+        return js;
+    }
+
+    public ArrayList<JSTest> getTests() {
+        return tests;
+    }
 }
