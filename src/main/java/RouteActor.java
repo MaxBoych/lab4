59feee4c0c178e4185582519ed3422143d56bcff
maxboychuk <maxboydad@gmail.com>
2019-11-22 01:58:44 +0300

    Scripted auto-commit on change (2019-11-22 01:58:44) by gitwatch.sh

--- src/main/java/RouteActor.java
+++ src/main/java/RouteActor.java
@@ -20,7 +20,9 @@ public class RouteActor extends AbstractActor {
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(JSToObject.class, message -> {
-                    
+                    for (JSTest test : message.getTests()) {
+                        
+                    }
                 })
     }
 }
