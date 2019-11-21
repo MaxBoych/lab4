daec145de4ad5e3336a66cb2acc38c964e9e8fde
maxboychuk <maxboydad@gmail.com>
2019-11-21 23:29:40 +0300

    Scripted auto-commit on change (2019-11-21 23:29:40) by gitwatch.sh

--- src/main/java/JSRouter.java
+++ src/main/java/JSRouter.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import akka.http.javadsl.server.Route;
+
+import static akka.http.javadsl.server.Directives.route;
 
 public class JSRouter {
+
+    private Route jsRoute() {
+        return route();
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,13 @@ import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.server.Route;
 
+import static akka.http.javadsl.server.Directives.route;
+
 public class Main {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("main");
         ActorRef mainActor = system.actorOf(Props.create(MainActor.class));
     }
 
-    private Route jsRoute() {
-        return ;
-    }
+
 }
