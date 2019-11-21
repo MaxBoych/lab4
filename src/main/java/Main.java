b176619b3ceb0bcec81294a3d8a7c02b9a418d14
maxboychuk <maxboydad@gmail.com>
2019-11-22 00:13:18 +0300

    Scripted auto-commit on change (2019-11-22 00:13:18) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,19 @@
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.Http;
 import akka.http.javadsl.server.Route;
+import akka.stream.ActorMaterializer;
 
 import static akka.http.javadsl.server.Directives.route;
 
 public class Main {
     public static void main(String[] args) {
+
         ActorSystem system = ActorSystem.create("main");
         ActorRef mainActor = system.actorOf(Props.create(MainActor.class));
+
+        Http http = Http.get(system);
+        ActorMaterializer materializer = ActorMaterializer.create(system);
     }
-
-
 }
