1e18c610a3e34af5ecf181cffc4b91cdc00104b5
maxboychuk <maxboydad@gmail.com>
2019-11-22 01:36:36 +0300

    Scripted auto-commit on change (2019-11-22 01:36:36) by gitwatch.sh

--- src/main/java/JSTest.java
+++ src/main/java/JSTest.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+
 public class JSTest {
 
     private String testName;
-    
+    private String expected;
+    private ArrayList<Object> params;
+
+    public JSTest(String testName, String expected, ArrayList<Object> params) {
+        this.testName = testName;
+        this.expected = expected;
+        this.params = params;
+    }
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public String getExpected() {
+        return expected;
+    }
+
+    public ArrayList<Object> getParams() {
+        return params;
+    }
 }
