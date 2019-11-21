cd483ee3ce4bb90621875af61d68275e09c563ad
maxboychuk <maxboydad@gmail.com>
2019-11-22 00:54:04 +0300

    Scripted auto-commit on change (2019-11-22 00:54:04) by gitwatch.sh

--- src/main/java/RouteActor.java
+++ src/main/java/RouteActor.java
@@ -1,5 +1,8 @@
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.japi.pf.ReceiveBuilder;
+import akka.routing.RoundRobinPool;
 
 public class RouteActor extends AbstractActor {
 
@@ -7,11 +10,17 @@ public class RouteActor extends AbstractActor {
     private ActorRef storageActor;
 
     public RouteActor() {
-        executeActor = getContext().actorOf(new RoundeRobinPool())
+        executeActor = getContext().actorOf(new RoundRobinPool(5)
+                .props(Props.create(ExecuteActor.class)));
+
+        storageActor = getContext().actorOf(Props.create(StoreActor.class));
     }
 
     @Override
     public Receive createReceive() {
-        return null;
+        return ReceiveBuilder.create()
+                .match(JSToObject.class, message -> {
+                    long length = 
+                })
     }
 }
