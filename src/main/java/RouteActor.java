109fe55b190654ebfdc346b607ccc52e6f25e25c
maxboychuk <maxboydad@gmail.com>
2019-11-22 00:48:50 +0300

    Scripted auto-commit on change (2019-11-22 00:48:50) by gitwatch.sh

--- src/main/java/RouteActor.java
+++ src/main/java/RouteActor.java
@@ -1,8 +1,14 @@
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
 
 public class RouteActor extends AbstractActor {
 
-    private 
+    private ActorRef executeActor;
+    private ActorRef storageActor;
+
+    public RouteActor() {
+        executeActor = getContext().actorOf(new RoundeRobinPool())
+    }
 
     @Override
     public Receive createReceive() {
