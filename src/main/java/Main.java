8484118bf967ae93b56defc3768099c9350aec02
maxboychuk <maxboydad@gmail.com>
2019-11-21 23:19:01 +0300

    Scripted auto-commit on change (2019-11-21 23:19:01) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 
 public class Main {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("main");
-
+        ActorRef mainActor = system.actorOf(Props.create(MainActor.class));
     }
+    
+
 }
