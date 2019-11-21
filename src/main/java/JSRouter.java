38cfa0d1c9fca8196f1ec9680fc3ccf083ebd25b
maxboychuk <maxboydad@gmail.com>
2019-11-21 23:30:27 +0300

    Scripted auto-commit on change (2019-11-21 23:30:27) by gitwatch.sh

--- src/main/java/JSRouter.java
+++ src/main/java/JSRouter.java
@@ -1,12 +1,13 @@
 import akka.http.javadsl.server.Route;
 
+import static akka.http.javadsl.server.Directives.get;
 import static akka.http.javadsl.server.Directives.route;
 
 public class JSRouter {
 
     public Route jsRoute() {
         return route(
-                
+                get()
         );
     }
 }
