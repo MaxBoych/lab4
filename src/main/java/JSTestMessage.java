a5896248321a3182dfa91188457b31dfe2152fb9
maxboychuk <maxboydad@gmail.com>
2019-11-22 21:04:48 +0300

    Scripted auto-commit on change (2019-11-22 21:04:48) by gitwatch.sh

--- src/main/java/JSTestMessage.java
+++ src/main/java/JSTestMessage.java
@@ -5,5 +5,28 @@ public class JSTestMessage {
     private String expected;
     private String functionName;
     private JSTest test;
+
+    public JSTestMessage(String testName, String expected,
+                         String functionName, JSTest test) {
+        this.testName = testName;
+        this.expected = expected;
+        this.functionName = functionName;
+        this.test = test;
+    }
     
+    public String getTestName() {
+        return testName;
+    }
+
+    public String getExpected() {
+        return expected;
+    }
+
+    public String getFunctionName() {
+        return functionName;
+    }
+
+    public JSTest getTest() {
+        return test;
+    }
 }
