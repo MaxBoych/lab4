0cddf2bac2894fb73080898d4609c136a13e398f
maxboychuk <maxboydad@gmail.com>
2019-11-23 00:14:25 +0300

    Scripted auto-commit on change (2019-11-23 00:14:25) by gitwatch.sh

--- src/main/java/TestExecuteActor.java
+++ src/main/java/TestExecuteActor.java
@@ -6,6 +6,6 @@ public class TestExecuteActor extends AbstractActor {
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match()
+                .match(JSTestMessage)
     }
 }
