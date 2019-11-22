b28b1949133805f4ce7698be8dbf524f92f5f547
maxboychuk <maxboydad@gmail.com>
2019-11-23 00:13:17 +0300

    Scripted auto-commit on change (2019-11-23 00:13:17) by gitwatch.sh

--- src/main/java/TestExecuteActor.java
+++ src/main/java/TestExecuteActor.java
@@ -1,2 +1,9 @@
-public class TestActor {
+import akka.actor.AbstractActor;
+
+public class TestExecuteActor extends AbstractActor {
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
