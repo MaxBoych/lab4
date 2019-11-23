d8668818f632bc3e0820b00240bb82af4598f18b
maxboychuk <maxboydad@gmail.com>
2019-11-23 10:38:59 +0300

    Scripted auto-commit on change (2019-11-23 10:38:59) by gitwatch.sh

--- src/main/java/JSObject.java
+++ src/main/java/JSObject.java
@@ -1,12 +1,21 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class JSObject {
 
+    @JsonProperty(Config.PACKAGE_ID)
     private String packageId;
+
+    @JsonProperty(Config.JS_SCRIPT)
     private String jsScript;
+
+    @JsonProperty(Config.FUNCTION_NAME)
     private String functionName;
+
+    @JsonProperty(Config.TEST)
     private JSTest[] tests;
 
     public JSObject(String testName, String js,
