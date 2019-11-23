9db5368458247c1350f3782f038549cc80cb161d
maxboychuk <maxboydad@gmail.com>
2019-11-23 10:31:53 +0300

    Scripted auto-commit on change (2019-11-23 10:31:53) by gitwatch.sh

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -3,4 +3,10 @@ public class Config {
     public static final String PACKAGE_ID = "packageId";
     public static final String JS_SCRIPT = "jsScript";
     public static final String FUNCTION_NAME = "functionName";
+    public static final String TESTS = "tests";
+    public static final String TEST = "test";
+    public static final String EXPECTED_RESULT = "expectedResult";
+    public static final String PARAMS = "params";
+    public static final String RESULT = "result";
+    public static final String RESULT = "result";
 }
